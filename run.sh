bc64647b36e96c7da8ac02bb188b76ba465e86eb
Demian <dembanakh01@gmail.com>
2022-02-11 22:35:00 +0100

    update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 cargo build
-cargo run -p voting-server -- A,B &
+cargo run -p voting-server -- 3 2 A,B &
 RUST_LOG=$1 cargo run -p voting-system -- 0 & 
 RUST_LOG=$1 cargo run -p voting-system -- 1 &
 RUST_LOG=$1 cargo run -p voting-system -- 2 &
